07ded4611f9503cc647a5c9ccb36bef14f4256d7
ThiagoJBarbosa <thiagojesusbs@gmail.com>
2023-10-10 23:56:41 -0300

    Estilizando a home com o SASS e alterando a imagem

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -10,8 +10,8 @@ export default function Cabecalho(){
             <header className="cabecalho">
                 <nav>
                     <ul>
-                        <li><Link to="/" className={ rotaAtual.pathname == "/" ? "active" : ""}>Home</Link></li>
-                        <li><Link to="/produtos" className={ rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
+                        <li className="homebutton"><Link to="/" className={ rotaAtual.pathname == "/" ? "active" : ""}>Home</Link></li>
+                        <li className="Produtobutton"><Link to="/produtos" className={ rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
                     </ul>
                 </nav>
             </header>
